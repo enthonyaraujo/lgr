a7e6b9161f2b34cdcfc1ec3e0ad4380c76def65a
Enthony Araujo de Oliveira <enthonyaraujo01@gmail.com>
2026-09-01 10:46:31 -0300

    fix(android): respeitar area util da tela (safe-area/fitsSystemWindows) e evitar sobreposicao com barra de status

--- android/app/src/main/java/br/com/enthony/lgrstudio/MainActivity.java
+++ android/app/src/main/java/br/com/enthony/lgrstudio/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.enthony.lgrstudio;
 
 import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +9,8 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(LgrPythonPlugin.class);
         super.onCreate(savedInstanceState);
+
+        // Garante que o WebView e o app respeitem a area util (sem ficar por baixo da barra de status)
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
     }
 }
